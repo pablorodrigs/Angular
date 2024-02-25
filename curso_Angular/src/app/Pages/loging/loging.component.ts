05cb8ce52ff0c9a80dd6929119fb8f7b49b5cfdf
Pablorodrigs <pablorodrigs6@gmail.com>
2024-02-25 18:09:14 -0300

    Adicionando novos recursos

---
+++ curso_Angular/src/app/Pages/loging/loging.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loging',
+  standalone: true,
+  imports: [],
+  templateUrl: './loging.component.html',
+  styleUrl: './loging.component.css'
+})
+export class LogingComponent {
+
+}
